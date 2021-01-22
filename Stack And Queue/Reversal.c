9ec1334287399b836328402e42799f554976605d
Nji Daniel <njid18753@gmail.com>
2021-01-22 05:50:06 +0100

    Update Reversal.c

--- Stack And Queue/Reversal.c
+++ Stack And Queue/Reversal.c
@@ -1,4 +1,4 @@
-//Count the reversal
+/*Count the reversal
 Given a string S consisting only of opening and closing curly brackets "{" , "}".find out the minimum
 number of reversals required to make a balanced expression.
 
